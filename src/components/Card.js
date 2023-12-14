c8c0b291babf21fcb76e75e0759c8d6821642355
Siddharth Joisar <sidjoisar0807@gmail.com>
2023-12-15 01:05:25 +0530

    done

---
+++ src/components/Card.js
@@ -0,0 +1,43 @@
+// import React, { Component } from 'react'
+import React from 'react';
+import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
+
+const Card = (props) => {
+
+  let review = props.review;
+
+  return (
+    <div className='flex flex-col md:relative'>
+
+      <div className='absolute top-[-7rem] z-[10] mx-auto'>
+        <img
+          className="aspect-square rounded-full w-[170px] h-[180px] z-[25]"
+          src={review.image} alt='noimage' />
+
+        {/* <div className='w-[140px] h-[140px] bg-violet-500 rounded-full absolute top-[-6px] [-10] left-[10px]'>
+        </div> */}
+
+      </div>
+
+      <div className='text-center mt-7'>
+        <p className='tracking-wider font-bold text-2xl capitalize'>{review.name}</p>
+
+        <p className='text-violet-700 font-medium uppercase text-sm'>{review.job}</p>
+      </div>
+
+      <div className='text-violet-300 mx-auto mt-5'>
+        <FaQuoteLeft />
+      </div>
+
+      <div className='text-center mt-4 text-slate-500'>
+        {review.text}
+      </div>
+
+      <div className='text-violet-300 mx-auto mt-5'>
+        <FaQuoteRight />
+      </div>
+    </div>
+  )
+}
+
+export default Card;
---
+++ src/data.js
@@ -0,0 +1,39 @@
+const reviews = [
+  {
+    id: 1,
+    name: "Siddharth Joisar",
+    job: "Software Engineer",
+    image: "images/Siddharth_Joisar.jpg",
+    text: "I have had the pleasure of working with this team on several projects, and I am consistently impressed with their technical expertise and ability to deliver quality solutions on time and within budget. They are a true partner and an asset to any project.",
+  },
+  {
+    id: 2,
+    name: "John Keller",
+    job: "Graphic Designer",
+    image: "images/img1.jpg",
+    text: "I have been working with this company for several years now, and I have always been impressed with their creativity and attention to detail. They are true professionals who take pride in their work and always go above and beyond to deliver exceptional results.",
+  },
+  {
+    id: 3,
+    name: "Ana Smith",
+    job: "Marketing Manager",
+    image: "images/img2.jpg",
+    text: "I am thrilled with the results of our recent marketing campaign, and it wouldn't have been possible without the hard work and dedication of the entire team. Thank you for your exceptional work!",
+  },
+  {
+    id: 4,
+    name: "Dev Sharma",
+    job: "Content Strategist",
+    image: "images/img3.jpg",
+    text: "I have worked with many content creators over the years, but none have impressed me as much as this team. They have a knack for crafting compelling and engaging content that resonates with our audience and drives real results. I highly recommend them!",
+  },
+  {
+    id: 5,
+    name: "Roger Nadal",
+    job: "Data Analyst",
+    image: "images/img4.jpg",
+    text: "I had the opportunity to work with this team on a data analysis project, and I was impressed with their attention to detail and their ability to work with complex datasets. They provided valuable insights that helped us make informed business decisions. It was a pleasure working with them.",
+  },
+];
+
+export default reviews;
